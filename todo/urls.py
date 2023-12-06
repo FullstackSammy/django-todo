22ee06662ed59a7bc6f53e36708be1ccaad84a0c
samzzii <nasstromsamuel@icloud.com>
2023-12-06 17:48:26 +0100

    added view for tasks

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TaskListView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('tasks/', TaskListView.as_view(), name='tasks'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Task
 
 # Create your views here.
 
 class HomeView(TemplateView):
+    model = Task
     template_name = 'todo/index.html'
     
+
+class TaskListView(ListView):
+    model = Task
     
