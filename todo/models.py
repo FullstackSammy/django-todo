e07618c831c07b806f5b7988aa53777876085d26
samzzii <nasstromsamuel@icloud.com>
2023-12-06 17:16:18 +0100

    added model for Tasks

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    PRIORITIES = (
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    )
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    priority = models.CharField(max_length=50, choices=PRIORITIES)
+    deadline = models.DateField(null=True)
+    completed = models.BooleanField(default=False)
