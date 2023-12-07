f40c3d7c35349ceabe2a6323e9e7f49ecedd714d
samzzii <nasstromsamuel@icloud.com>
2023-12-07 12:45:57 +0100

    added detail view for tasks

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, TaskListView
+from .views import HomeView, TaskListView, TaskDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('tasks/', TaskListView.as_view(), name='tasks'),
+    path('tasks/<int:pk>', TaskDetailView.as_view(), name='task-detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Task
 
 # Create your views here.
@@ -10,5 +10,9 @@ class HomeView(TemplateView):
     
 
 class TaskListView(ListView):
+    model = Task
+
+
+class TaskDetailView(DetailView):
     model = Task
     
