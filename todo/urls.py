64dcc2d35a081e9e36ee5431bf91e8f754bb81ec
samzzii <nasstromsamuel@icloud.com>
2023-12-07 13:15:56 +0100

    added urls and paths for editing tasks

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, TaskListView, TaskDetailView, TaskCreateView
+from .views import HomeView, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('tasks/', TaskListView.as_view(), name='tasks'),
-    path('tasks/create', TaskCreateView.as_view(), name='task-create'),
-    path('tasks/<int:pk>', TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/edit/<int:pk>/', TaskUpdateView.as_view(), name='task-edit'),
 ]
